316806dce0ae33bcf516424d26fe7000a8b636d7
Mayank Kumar <mynk8076@gmail.com>
2026-06-28 15:35:45 +0530

    setup pm2 and setup for auto utalize all cores

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,38 @@
+// module.exports = {
+//   apps: [
+//     {
+//       name: 'ecommerse-nest-backend',
+//       script: './dist/src/main.js',
+//       instances: 1, 
+//       exec_mode: 'fork',
+//       env: {
+//         NODE_ENV: 'production',
+//       },
+//     },
+//   ],
+// };
+
+
+
+const os = require('os');
+
+// Automatically check how many CPU cores the machine has
+const totalCores = os.cpus().length;
+
 module.exports = {
   apps: [
     {
       name: 'ecommerse-nest-backend',
       script: './dist/src/main.js',
-      instances: 1, 
-      exec_mode: 'fork',
+      
+      // 👇 AUTOMATIC ADJUSTMENT LOGIC
+      // If 1 core (Render Free Tier), use 1 instance. 
+      // If multi-core (Paid Tier), use all available cores ('max')!
+      instances: totalCores === 1 ? 1 : 'max', 
+      
+      // Automatically switch to 'cluster' mode only if there are multiple cores
+      exec_mode: totalCores === 1 ? 'fork' : 'cluster',
+      
       env: {
         NODE_ENV: 'production',
       },
